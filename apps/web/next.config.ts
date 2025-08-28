1ec864ba40f611d9d52d1b1303fed4d4d1c72bd3
swalha1999 <mohamd514.m@gmail.com>
2025-08-28 17:53:07 +0300

    added migrate before the build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	typedRoutes: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "estatemar.d97515014e92cd4f04045d814b2679c4.r2.cloudflarestorage.com",
+			}
+		]
+	},
 };
 
 export default nextConfig;
