1adb39b0c2f6fe82c86442530f91df6410a026d0
swalha1999 <mohamd514.m@gmail.com>
2025-08-28 18:03:14 +0300

    hardcodded the db sting

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,13 +1,10 @@
-import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-
-config({ path: ".env" });
 
 export default defineConfig({
 	schema: "./src/db/schema",
 	out: "./src/db/migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: process.env.DATABASE_URL || "",
+		url: process.env.DATABASE_URL || "postgresql://postgres:bxeo8vhm4btr2mpu@49.13.12.249:9999/estatemar",
 	},
 });
